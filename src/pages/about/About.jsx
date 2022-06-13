d96cb85be3475853bdc4740b8ce9047c4eb6ae7e
Stuti <stutijain2711@gmail.com>
2022-06-13 19:53:47 +0530

    Initial commit

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,34 @@
+import "./about.css";
+
+export default function About() {
+  return (
+    <div className="about">
+      
+      
+      <p className="aboutdes">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam? Lorem ipsum 
+        dolor, sit amet consectetur adipisicing elit. Fugit nulla consequuntur 
+        quibusdam possimus, quisquam vel ullam cupiditate dolorum? Officiis 
+        officia autem, in voluptatem minima iste placeat, reiciendis sed 
+        corporis perferendis veritatis facilis odio, animi sit deserunt ea
+        excepturi. Repellendus quisquam nihil excepturi delectus vero iusto
+        perspiciatis illo eveniet amet fuga. Lorem ipsum dolor sit, amet
+        consectetur adipisicing elit. Dolore atque unde aut, alias at commodi 
+        repellat fuga doloremque et, placeat sit laboriosam quo voluptates
+        dolorum est iure sint quidem beatae.
+      </p>
+      <br />
+      <p className="Contactdet">
+        CONTACT US: 9454030089
+        <br />
+        EMAIL ID: gccsrm@gmail.com
+      </p>
+
+
+      
+    </div>
+  );
+}
